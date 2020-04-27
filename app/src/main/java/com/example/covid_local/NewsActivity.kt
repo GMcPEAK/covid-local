88a19ed960507b5983002667058f432dd4be99f7
GMcPEAK <gradymcpeak@gmail.com>
2020-04-26 21:25:36 -0400

    Completed functionality

--- app/src/main/java/com/example/covid_local/NewsActivity.kt
+++ app/src/main/java/com/example/covid_local/NewsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.covid_local
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +22,7 @@ class NewsActivity : AppCompatActivity() {
 
     private val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
     private lateinit var recyclerView: RecyclerView
+    private lateinit var toSettings: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,13 @@ class NewsActivity : AppCompatActivity() {
         title = getString(R.string.title1)
         setContentView(R.layout.activity_news)
         recyclerView = findViewById(R.id.recyclerView)
+        toSettings = findViewById(R.id.toSettings)
         recyclerView.layoutManager = LinearLayoutManager(this@NewsActivity)
+
+        toSettings.setOnClickListener {
+            val intent = Intent(this@NewsActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
 
         lateinit var adapter: StoryAdapter
         try {
@@ -50,13 +62,17 @@ class NewsActivity : AppCompatActivity() {
     }
 
     fun getStories(): List<Story> {
-        var searchTerm = getSharedPreferences("project2-byoi-covid-local", 0).getString("country", "")
+        val preferences: SharedPreferences = getSharedPreferences(
+            "covid-local",
+            Context.MODE_PRIVATE
+        )
+        var searchTerm = preferences.getString("location", "")
         Log.d("NewsActivity", "Search term is $searchTerm")
         val stories = mutableListOf<Story>()
         // Create the Request object
         val key = getString(R.string.news_api_key)
         val request = Request.Builder()
-            .url("https://newsapi.org/v2/everything?q=\"coronavirus delaware\"&sortBy=recent&apiKey=$key")
+            .url("https://newsapi.org/v2/everything?q=\"coronavirus $searchTerm\"&sortBy=recent&apiKey=$key")
             .header("Authorization", "Bearer $key")
             .build()
         val response = okHttpClient.newCall(request).execute()
@@ -79,6 +95,7 @@ class NewsActivity : AppCompatActivity() {
                 val url = curr.getString("url")
                 val imgUrl = curr.getString("urlToImage")
                 val timeStamp = curr.getString("publishedAt")
+                val source = curr.getJSONObject("source").getString("name")
                 stories.add(
                     Story(
                         author = author,
@@ -86,12 +103,55 @@ class NewsActivity : AppCompatActivity() {
                         description = description,
                         url = url,
                         imgUrl = imgUrl,
-                        timeStamp = timeStamp
+                        timeStamp = timeStamp,
+                        source = source
                     )
                 )
             }
 
         }
+
+        val request2 = Request.Builder()
+            .url("https://newsapi.org/v2/everything?q=\"covid-19 $searchTerm\"&sortBy=recent&apiKey=$key")
+            .header("Authorization", "Bearer $key")
+            .build()
+        val response2 = okHttpClient.newCall(request2).execute()
+        // Get the JSON string body, if there was one
+        val responseString2 = response2.body?.string()
+        Log.d("NewsActivity", "$responseString2")
+        if (response2.isSuccessful && !responseString2.isNullOrEmpty()) {
+            // Parse our JSON string
+            val json = JSONObject(responseString2)
+            val storiesJSON = json.getJSONArray("articles")
+            var l = storiesJSON.length()
+            Log.d("LocationsActivity", "Length of return array = $l")
+            var size = l
+            for (i in 0 until size) {
+                val curr = storiesJSON.getJSONObject(i)
+                Log.d("LocationsActivity", "curr = $curr")
+                val author = curr.getString("author")
+                val title = curr.getString("title")
+                val description = curr.getString("description")
+                val url = curr.getString("url")
+                val imgUrl = curr.getString("urlToImage")
+                val timeStamp = curr.getString("publishedAt")
+                val source = curr.getJSONObject("source").getString("name")
+                stories.add(
+                    Story(
+                        author = author,
+                        title = title,
+                        description = description,
+                        url = url,
+                        imgUrl = imgUrl,
+                        timeStamp = timeStamp,
+                        source = source
+                    )
+                )
+            }
+
+        }
+
+
         for (story in stories) {
             var currName = story.title
             Log.d("Locations Activity:", "$currName")
--- app/src/main/java/com/example/covid_local/StoryAdapter.kt
+++ app/src/main/java/com/example/covid_local/StoryAdapter.kt
@@ -6,10 +6,7 @@ import android.opengl.ETC1.getHeight
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
@@ -18,36 +15,53 @@ import org.w3c.dom.Text
 
 class StoryAdapter(val stories: List<Story>) : RecyclerView.Adapter<StoryAdapter.ViewHolder>(),
     View.OnClickListener {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val title: TextView = itemView.findViewById(R.id.title)
-        val author: TextView = itemView.findViewById(R.id.author)
-        val descriptor: TextView = itemView.findViewById(R.id.description)
-        val im: ImageView = itemView.findViewById(R.id.storyImage)
-    }
+            val title: TextView = itemView.findViewById(R.id.title)
+            val author: TextView = itemView.findViewById(R.id.author)
+            val descriptor: TextView = itemView.findViewById(R.id.description)
+            val im: ImageView = itemView.findViewById(R.id.storyImage)
+            val readButton: Button = itemView.findViewById(R.id.readButton)
+            val source: TextView = itemView.findViewById(R.id.publication)
+        }
 
-    // The adapter needs to render a new row and needs to know what XML file to use
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        //Layout inflation: read, parse XML file and return a reference to the root layout
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.story, parent, false)
-        return ViewHolder(view)
-    }
+        // The adapter needs to render a new row and needs to know what XML file to use
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            //Layout inflation: read, parse XML file and return a reference to the root layout
+            val view: View = LayoutInflater.from(parent.context).inflate(R.layout.story, parent, false)
+            return ViewHolder(view)
+        }
 
-    //The adapter has a row that's ready to be rendered and needs the content filled in
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentStory = stories[position]
-        holder.title.text = currentStory.title
-        holder.author.text = currentStory.author
-        holder.descriptor.text = currentStory.description
-        Picasso.get().load(currentStory.imgUrl).resize(holder.im.measuredWidth, 450).centerCrop().into(holder.im)
-    }
+        //The adapter has a row that's ready to be rendered and needs the content filled in
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val currentStory = stories[position]
+            holder.title.text = currentStory.title
+            if (currentStory.author != null) {
+                holder.author.text = ("by " + currentStory.author)
+            } else {
+                holder.source.text = ""
+            }
+            holder.descriptor.text = currentStory.description
+            Picasso.get().load(currentStory.imgUrl).resize(holder.im.measuredWidth, 450).centerCrop().into(holder.im)
+            var cont = holder.readButton.getContext()
+            holder.readButton.setOnClickListener {
+                val intent = Intent(android.content.Intent.ACTION_VIEW)
+                intent.data = Uri.parse(currentStory.url)
+                startActivity(cont, intent, null)
+            }
+            if (currentStory.source != "") {
+                holder.source.text = (" for " + currentStory.source)
+            } else {
+                holder.source.text = ""
+            }
+        }
 
-    //Return the total number of rows you expect your list to have
-    override fun getItemCount(): Int {
-        return stories.size
-    }
+        //Return the total number of rows you expect your list to have
+        override fun getItemCount(): Int {
+            return stories.size
+        }
 
-    override fun onClick(v: View?) {
+        override fun onClick(v: View?) {
 
-    }
+        }
 }
